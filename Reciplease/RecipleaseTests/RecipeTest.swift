9e000d12e4826fd96cb96aa9a43ba61c300141ff
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2024-07-10 21:03:35 +0200

    Feat: creat Mock

--- Reciplease/RecipleaseTests/RecipeTest.swift
+++ Reciplease/RecipleaseTests/RecipeTest.swift
@@ -6,36 +6,43 @@
 //
 
 import XCTest
+@testable import Reciplease
+
+class MockRecipeURLProtocol: URLProtocol {
+    static var mockResponseData: Data?
+    static var mockError: Error?
+    static var mockResponse: HTTPURLResponse?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func stopLoading() {
+        if let data = MockRecipeURLProtocol.mockResponseData {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let error = MockRecipeURLProtocol.mockError {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        
+        if let response = MockRecipeURLProtocol.mockResponse {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    static func resetMock() {
+        MockRecipeURLProtocol.mockError = nil
+        MockRecipeURLProtocol.mockResponse = nil
+        MockRecipeURLProtocol.mockResponseData = nil
+    }
+}
 
 final class RecipeTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
 }
