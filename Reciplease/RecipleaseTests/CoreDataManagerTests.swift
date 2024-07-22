0836ca3e171b9f57ed7bf345c3392709559d6dc2
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2024-07-22 13:15:40 +0200

    Feat : removeToFavorite fail situation

--- Reciplease/RecipleaseTests/CoreDataManagerTests.swift
+++ Reciplease/RecipleaseTests/CoreDataManagerTests.swift
@@ -6,30 +6,131 @@
 //
 
 import XCTest
+import CoreData
+@testable import Reciplease
 
 final class CoreDataManagerTests: XCTestCase {
+    
+    var context: NSManagedObjectContext!
+    var recipeModel: CoreDataManager!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        
+        let persistentContainer = NSPersistentContainer(name: "Reciplease")
+        let description = NSPersistentStoreDescription()
+        description.type = NSInMemoryStoreType
+        persistentContainer.persistentStoreDescriptions = [description]
+        
+        persistentContainer.loadPersistentStores { description, error in
+            XCTAssertNil(error)
+        }
+        
+        context = persistentContainer.viewContext
+        recipeModel = CoreDataManager(context: context)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        context = nil
+        recipeModel = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testLoadRecipes() throws {
+        let recipe = RecipeEntity(context: context)
+        recipe.label = "Test Recipe"
+        
+        try context.save()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        recipeModel.loadRecipes()
+        
+        XCTAssertEqual(recipeModel.recipesList.count, 1)
+        XCTAssertEqual(recipeModel.recipesList.first?.label, "Test Recipe")
     }
-
+    
+    func testCheckFavoriteStatusEqualTrue() throws {
+        let recipeLabel = "Test Recipe"
+        let recipe = RecipeEntity(context: context)
+        recipe.label = recipeLabel
+            
+        try context.save()
+        
+        let isFavorite = recipeModel.checkFavoriteStatus(for: recipeLabel)
+        
+        XCTAssertTrue(isFavorite)
+    }
+    
+    func testCheckFavoriteStatusEqualFalse() throws {
+        let recipeLabel = "Test Recipe"
+        
+        let isFavorite = recipeModel.checkFavoriteStatus(for: recipeLabel)
+        
+        XCTAssertFalse(isFavorite)
+    }
+    
+    func testRemoveToFavoriteSuccess() throws {
+        let recipeLabel = "Test Recipe"
+        let recipe = RecipeEntity(context: context)
+        recipe.label = recipeLabel
+        
+        try context.save()
+        
+        let fetchRequest: NSFetchRequest<RecipeEntity> = RecipeEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "label == %@", recipeLabel)
+        
+        var results = try context.fetch(fetchRequest)
+        XCTAssertEqual(results.count, 1)
+        
+        recipeModel.removeToFavorite(for: recipeLabel)
+        
+        results = try context.fetch(fetchRequest)
+        XCTAssertEqual(results.count, 0)
+    }
+    
+    func testRemoveToFavoriteFail() throws {
+        let recipeLabel = "Test Recipe"
+        let recipe = RecipeEntity(context: context)
+        recipe.label = recipeLabel
+        
+        try context.save()
+        
+        let fetchRequest: NSFetchRequest<RecipeEntity> = RecipeEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "label == %@", recipeLabel)
+        
+        var results = try context.fetch(fetchRequest)
+        XCTAssertEqual(results.count, 1)
+        
+        recipeModel.removeToFavorite(for: "recipeLabel")
+        
+        results = try context.fetch(fetchRequest)
+        XCTAssertEqual(results.count, 1)
+    }
+    
+    func testAddToFavorite() throws {
+        
+        let recipe = Recipe(
+            label: "Recipe Test",
+            image: "www.testUrl.fr",
+            shareAs: "www.testUrl.fr",
+            ingredients: [Ingredient(text: "ingredients1", quantity: 0.0, measure: nil, food: "ingredients", weight: 0.0, foodCategory: "food", foodID: "0", image: nil)],
+            calories: 30.99,
+            totalTime: 43
+        )
+        
+        let testRecipe = RecipeRepresentable(recipe: recipe)
+        
+        recipeModel.addToFavorite(for: testRecipe)
+        
+        let fetchRequest: NSFetchRequest<RecipeEntity> = RecipeEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "label == %@", testRecipe.recipeTitle)
+            
+        let results = try context.fetch(fetchRequest)
+        
+        XCTAssertEqual(results.count, 1)
+        XCTAssertEqual(results.first?.label, testRecipe.recipeTitle)
+        XCTAssertEqual(results.first?.calories, testRecipe.calorie)
+        XCTAssertEqual(results.first?.time, Int32(testRecipe.time ?? 0))
+        XCTAssertEqual(results.first?.ingredients, testRecipe.ingredients)
+        XCTAssertEqual(results.first?.shareAs, testRecipe.shareAs)
+    }
 }
